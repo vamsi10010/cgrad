8d920db8638515d0dc837aa43e164fb316180eb1
Vamsi Raman Deeduvanu <vdeeduva@purdue.edu>
2023-08-11 13:30:45 -0500

    made some structs

---
+++ src/grad.h
@@ -0,0 +1,38 @@
+#ifndef __GRAD_H__
+#define __GRAD_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef enum operation_enum {
+    ADD,
+    MUL,
+    POW,
+    RELU,
+    TANH
+} OPERATION; 
+
+typedef struct value_struct {
+    double val;
+    double grad;
+    bool requires_grad;
+    void (* backward)(struct value_struct *);
+    OPERATION op;
+    struct value_struct *left;
+    struct value_struct *right;
+} VALUE;
+
+typedef struct node_struct {
+    VALUE value;
+    struct node_struct *prev;
+} NODE;
+
+
+
+
+
+
+
+#endif // __GRAD_H__
